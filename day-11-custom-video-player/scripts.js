c327e4764c5bb029fac58b7260076e3dd9d1fe45
Joao Martins <joaomarcelofm@gmail.com>
2018-02-01 22:24:13 +0100

    Scrub funtion

--- day-11-custom-video-player/scripts.js
+++ day-11-custom-video-player/scripts.js
@@ -28,6 +28,16 @@ function rangeUpdateAction() {
   video[this.name] = this.value
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+}
+
 // Actions
 
 video.addEventListener('click', togglePlay);
@@ -40,3 +50,11 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', rangeUpdateAction));
 ranges.forEach(range => range.addEventListener('mousemove', rangeUpdateAction));
+
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
