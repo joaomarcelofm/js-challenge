c6e68810f81ab4b82aefdf268c09c8dc47f2b1e3
Joao Martins <joaomarcelofm@gmail.com>
2018-02-01 22:06:49 +0100

    Added range functions

--- day-11-custom-video-player/scripts.js
+++ day-11-custom-video-player/scripts.js
@@ -7,7 +7,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 
 const togglePlayer = player.querySelector('.toggle');
-const ranges = player.querySelector('.player__slider')
+const ranges = player.querySelectorAll('.player__slider')
 const skipButtons = player.querySelectorAll('[data-skip]');
 
 // Functions
@@ -16,7 +16,7 @@ function togglePlay() {
   video.paused ? video.play() : video.pause()
 }
 
-function toggleButton() {
+function buttonUpdate() {
   video.paused ? togglePlayer.innerText = '►' : togglePlayer.innerText = '❚ ❚'
 }
 
@@ -24,12 +24,19 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip)
 }
 
+function rangeUpdateAction() {
+  video[this.name] = this.value
+}
+
 // Actions
 
 video.addEventListener('click', togglePlay);
 togglePlayer.addEventListener('click', togglePlay);
 
-video.addEventListener('play', toggleButton);
-video.addEventListener('pause', toggleButton);
+video.addEventListener('play', buttonUpdate);
+video.addEventListener('pause', buttonUpdate);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', rangeUpdateAction));
+ranges.forEach(range => range.addEventListener('mousemove', rangeUpdateAction));
