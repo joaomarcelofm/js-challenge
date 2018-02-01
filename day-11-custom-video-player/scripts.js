54ec339b63bfad8aa5b9117a09c48085bca07c48
Joao Martins <joaomarcelofm@gmail.com>
2018-02-01 21:14:07 +0100

    Selected player functions

---
+++ day-11-custom-video-player/scripts.js
@@ -0,0 +1,9 @@
+const player = document.querySelector('.player');
+const viewer = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const togglePlay = player.querySelector('.toggle');
+const ranges = player.querySelector('.player__slider')
+const skipButtons = player.querySelectorAll('data-skip');
